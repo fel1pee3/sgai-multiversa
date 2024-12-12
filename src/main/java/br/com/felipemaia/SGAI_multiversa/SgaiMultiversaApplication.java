6b2e8d51e3ed217db66da4c9574d9ba74cabaebb
Felipe Maia <132314205+fel1pee3@users.noreply.github.com>
2024-12-12 09:02:13 -0300

    first commit

---
+++ src/main/java/br/com/felipemaia/SGAI_multiversa/SgaiMultiversaApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipemaia.SGAI_multiversa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaiMultiversaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaiMultiversaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipemaia/SGAI_multiversa/SgaiMultiversaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipemaia.SGAI_multiversa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaiMultiversaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
